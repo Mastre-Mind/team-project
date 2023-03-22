df6b8b4c92784e25350a486bd62ecec2d10b90c3
Leventelg <123192718+Leventelg@users.noreply.github.com>
2023-03-22 13:31:23 -0700

    add p1 true variable

--- main.ts
+++ main.ts
@@ -3,3 +3,4 @@ let p1 = sprites.create(assets.image`p1WalkRight`, SpriteKind.Player)
 tiles.placeOnTile(p1, tiles.getTileLocation(1, 3))
 let p2 = sprites.create(assets.image`p2WalkLeft`, SpriteKind.Enemy)
 tiles.placeOnTile(p2, tiles.getTileLocation(8, 3))
+let p1Turn= true
