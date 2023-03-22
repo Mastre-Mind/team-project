405b58402afe35f8015ff669f76312f8bfcc3f7b
Mastre-Mind <128619928+Mastre-Mind@users.noreply.github.com>
2023-03-22 13:34:50 -0700

    setting variables

--- main.ts
+++ main.ts
@@ -3,4 +3,7 @@ let p1 = sprites.create(assets.image`p1WalkRight`, SpriteKind.Player)
 tiles.placeOnTile(p1, tiles.getTileLocation(1, 3))
 let p2 = sprites.create(assets.image`p2WalkLeft`, SpriteKind.Enemy)
 tiles.placeOnTile(p2, tiles.getTileLocation(8, 3))
-let p1Turn= true
+let p1Move = true
+let p1Attack = false
+let p2Move = false
+let p2Attack = false
