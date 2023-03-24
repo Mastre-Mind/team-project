cf30224a94f91541776e0b16d4fb0432c9b2306a
Mastre-Mind <128619928+Mastre-Mind@users.noreply.github.com>
2023-03-24 08:21:23 -0700

    Update main.ts

--- main.ts
+++ main.ts
@@ -1,4 +1,5 @@
 let list = [tilemap`first map`, tilemap`first map`]
+tiles.setCurrentTilemap(list[randint(0,2)])
 let p1 = sprites.create(assets.image`p1WalkRight`, SpriteKind.Player)
 tiles.placeOnTile(p1, tiles.getTileLocation(1, 3))
 let p2 = sprites.create(assets.image`p2WalkLeft`, SpriteKind.Enemy)
