001cd1667d8392d8384ea8d30f478247b0405a07
Mastre-Mind <128619928+Mastre-Mind@users.noreply.github.com>
2023-03-24 08:18:42 -0700

    list, movement

--- main.ts
+++ main.ts
@@ -1,9 +1,7 @@
-tiles.setCurrentTilemap(tilemap`first map`)
+let list = [tilemap`first map`, tilemap`first map`]
 let p1 = sprites.create(assets.image`p1WalkRight`, SpriteKind.Player)
 tiles.placeOnTile(p1, tiles.getTileLocation(1, 3))
 let p2 = sprites.create(assets.image`p2WalkLeft`, SpriteKind.Enemy)
 tiles.placeOnTile(p2, tiles.getTileLocation(8, 3))
-let p1Move = true
-let p1Attack = false
-let p2Move = false
-let p2Attack = false
+controller.player1.moveSprite(p1, 0, 50)
+controller.player2.moveSprite(p2, 0, 50)
